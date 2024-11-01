f707379c681bd08c9b705429f7997aca69c998e9
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-10-31 23:11:37 -0300

    to fix again: bloody SDL library imporation failed again

--- Source/Src/init_SDL_screen.cpp
+++ Source/Src/init_SDL_screen.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include "../Headers/init_SDL_screen.hpp"
+#include <SDL3/SDL.h>
+#include "../Headers/init_SDL_screen.h"
 
 using namespace std;
 
 struct{
     u_int32_t flg_video = SDL_INIT_VIDEO;
     u_int32_t flg_audio = SDL_INIT_AUDIO;
-    u_int32_t flg_controller = SDL_INIT_GAMECONTROLLER;
+    // u_int32_t flg_controller = SDL_INIT_GAMECONTROLLER;
 } flags_sdl;
+
+bool init_sdl_subsystem(){
+    u_int32_t result_flag = (
+        flags_sdl.flg_video | flags_sdl.flg_audio // | flags_sdl.flg_controller
+    );
+    return SDL_Init(result_flag);
+}
 
 bool setup(){
     bool result_initialize = false;
@@ -16,11 +23,4 @@ bool setup(){
     init_sdl_subsystem();
     // return screen config well done
     return result_initialize;
-}
-
-bool init_sdl_subsystem(){
-    u_int32_t result_flag = (
-        flags_sdl.flg_video | flags_sdl.flg_audio | flags_sdl.flg_controller
-    );
-    return SDL_Init(result_flag);
 }
