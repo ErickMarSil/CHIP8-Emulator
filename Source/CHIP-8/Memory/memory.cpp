1a25647d6f4083ea89150a46d585d2ff7989f1b5
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-11-04 22:57:09 -0300

    setted: structures

--- Source/CHIP-8/Memory/memory.cpp
+++ Source/CHIP-8/Memory/memory.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstdiny>
 
 using namespace std;
 
-using byte = unsigned char; // byte type variable
+typedef uint8 = byte; // byte type variable
+typedef uint16[16]; // 16 bytes type
+typedef uint16[32]; // 32 bytes type
 
 int MAX_MEMORY = 4096; // max memory for slots
+int STACK_MEMORY = 64; // stack memory
 int ZERO_PAGE_SPACE = 512; // memory to cpu registers 
 int DISPLAY_SPACE = 256; // memory to display
 int CALL_STACK_SPACE = 96; // memory to cpu call stack
@@ -17,13 +21,28 @@ Memory& get_memory(){ // return new memory object
 private: struct Memory{ // memory structure to inherit
     // WARNING: REMOVE AFTER, SOME PROGRAMS USES OR NOT THESE SPACES, THIS APPROACH CAN INTERFER IN IT
 
-    byte slots_memory[MAX_MEMORY - ZERO_PAGE_SPACE - DISPLAY_SPACE - CALL_STACK_SPACE]; // all memory slots removing pre-defined space memory
+    byte slots_memory[MAX_MEMORY - STACK_MEMORY - ZERO_PAGE_SPACE - DISPLAY_SPACE - CALL_STACK_SPACE]; // all memory slots removing pre-defined space memory
     byte slots_zero_page[ZERO_PAGE_SPACE]; // zero page for cpu space 
     byte slots_display[DISPLAY_SPACE]; // display space for display updates
     byte slots_call_stack[CALL_STACK_SPACE]; // call stack for stack access from cpu
+    byte 
+
+    using opcode = Opcode();
 
     // Functions
+    void clear_memory(){
+        for (int m, z, d, c, s; (m + z + d + c + s) < MAX_MEMORY; m++){ // clear all memorys slots in all lists
+            slots_memory[m] = 0x0;
+            slots_zero_page[z++] = 0x0;
+            slots_display[d++] = 0x0;
+            slots_call_stack[c++] = 0x0;   
+        }
+    }
 
-    
+}
 
+private : struct Opcode{
+    byte RET(Memory* mem, u_16* SP){
+        return;
+    }
 }
