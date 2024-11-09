6bb44854b3135b0a188dc9504adbbdb9d9e74614
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-11-08 22:43:11 -0300

    added: Memory functions in MEMORY.cpp and all includes to imports in MEMORY_H.h

--- Include/CHIP-8/BUS_H.h
+++ Include/CHIP-8/BUS_H.h
@@ -1,5 +1,18 @@
 #ifndef BUS_H
 #define BUS_H
 
+#include <cstdint>
+#include <MEMORY_H.h>
+
+public:
+    /* Read and write from slots stack */
+    uint8_t read_stack(uint16_t& addr);
+    void write_stack(uint16_t& addr, uint8_t data, uint8_t& SP);
+
+    /* Read and write from slots in framebuffer listener */
+    uint8_t read_framebuffer(uint16_t addr);
+    void write_framebuffer(uint16_t& addr, uint8_t data)
+
+    
 
 #endif
--- Include/CHIP-8/MEMORY_H.h
+++ Include/CHIP-8/MEMORY_H.h
@@ -1,25 +1,79 @@
 #include <cstdint>
+#include <iostream>
 #ifndef MEMORY_H
 #define MEMORY_H
 
-constexpr uint64_t MAX_MEMORY = 1024*4;
-constexpr uint8_t MAX_STACK = 16*2;
+private:
+    constexpr uint64_t MAX_MEMORY = 1024*4;
+    constexpr uint64_t MAX_FRAMEBUFFER = 1024;
+    constexpr uint8_t  MAX_STACK = 16*2;
+    constexpr uint8_t  MAX_KEYBOARDS = 16*2;
+    constexpr uint8_t  MAX_CONTROLDATA = 16*2;
 
-struct MEMORY {
-    /*
-    RAM slots begin to end 0x0 -> 0x1000 (4096 bytes)
-    STACK slots begin 0xEA0 to 0xEFF (32 bytes)
-    */
-    uint8_t MEMORY::RAM[MAX_MEMORY];
-    uint8_t MEMORY::STACK[MAX_STACK];
-    /*
-    CLEAR all memory slots from 0x0 to 0x1000
-    */
-    void MEMORY::clear_memory();
-    /*
-    INIT memory reseting it´s value
-    */
-    void MEMORY::init_memory();
-};
+public:
+    /*Enum others type*/
+    enum FETCH_TYPE:
+        fFmBuff = &Fetch_FrameBuffer;
+        fKeyB = &Fetch_KeyBoard;
+        fControllD = &Fetch_ControllData;
+    enum WRITE_TYPE:
+        wFmBuff = &Write_FrameBuffer;
+        wKeyB = &Write_KeyBoard;
+        wControllD = &Write_ControllData;
+
+    /*Enum memory type*/
+    enum FETCH_MEMORY:
+        fRAM = ;
+        fSTACK = ;
+    enum WRITE_MEMORY:
+        fRAM = ;
+        fSTACK = ;
+
+    struct MEMORY {
+        /*
+        RAM slots begin to end 0x0 -> 0x1000 (4096 bytes)
+        STACK slots begin 0xEA0 to 0xEFF (32 bytes)
+        */
+        uint8_t MEMORY::RAM[MAX_MEMORY]; /*RAM or HEAP memory to program it self*/
+        uint8_t MEMORY::STACK[MAX_STACK]; /*STACK or WRITE memory to cpu process and fast access*/
+        uint8_t MEMORY::FRAMEBUFFER[MAX_FRAMEBUFFER]; /*FRAMEBUFFER or DISPLAY to storage pixels*/
+        uint8_t MEMORY::KEYBOARD[MAX_KEYBOARDS]; /*KEYBOARD STATUS or INPUT handle the user inputs*/
+        uint8_t MEMORY::CONTROLDATA[MAX_CONTROLDATA]; /**/
+        /*
+        Clear all memorys types such as RAM, STACK, FRAMEBUFFER
+        */
+        void Reset_All_Memory();
+
+        /*Reset the Ram/Heap memory setting 0x00 bytes on it*/
+        void Reset_Memory();
+        /*Reset the Stack memory setting 0x00 bytes on it*/
+        void Reset_Stack();
+        /*Reset the Framebuffer bucket setting 0x00 bytes on it*/
+        void Reset_FrameBuffer();
+        /*Reset the Keyboard inputs setting 0x00 bytes on it*/
+        void Reset_KeyBoardS();
+
+        /*
+            Write/Fetch functions to differents buffer wires
+        */
+        private:
+            void Fetch_FrameBuffer(uint8_t& addr);  void Write_FrameBuffer(uint8_t& addr, uint8_t data);
+            void Fetch_KeyBoard(uint8_t& addr);     void Write_KeyBoard(uint8_t& addr, uint8_t data);
+            void Fetch_ControllData(uint8_t& addr); void Write_ControllData(uint8_t& addr, uint8_t data);
+
+        /*  Can read byte from others slots:
+                * FrameBuffer
+                * Keyboard
+                * ControlData*/
+        uint8_t FetchOthers(SLOTS_TYPE area_type, uint8_t& addr);
+
+        /*  Can write byte from others slots:
+                * FrameBuffer
+                * Keyboard
+                * ControlData*/
+        void WriteOthers(SLOTS_TYPE area_type, uint8_t& addr, uint16_t data);
+
+
+    };
 
 #endif
