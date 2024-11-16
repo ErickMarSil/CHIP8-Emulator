0ab1034b07fdcb15d3e7cc6f06bc2352158f00f2
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-11-16 20:17:17 -0300

    added: clock functionality, Opcode declaration | to_fix: fetch memory system

--- Include/CPU.h
+++ Include/CPU.h
@@ -11,43 +11,37 @@ class CPU
         CPU();
         ~CPU();
 
-        uint8_t VRegs[16]; /* Register V0 to VF */
-        uint16_t IR; /* Register "Index Register" */
-        uint8_t SP; /* Register "Stack Pointer" */
-        uint8_t DT; /* Register "Delay Timer" */
-        uint8_t ST; /* Register "Sounder Timer" */
-        uint16_t* PC; /* Register "Pointer Counter" */
+        uint8_t VRegs[16]; // Register V0 to VF
+        uint16_t I; // Register "Index Register"
+        uint8_t SP; // Register "Stack Pointer"
+        uint8_t DT; // Register "Delay Timer"
+        uint8_t ST; // Register "Sounder Timer"
+        uint16_t PC;// Register "Pointer Counter"
 
         /*OPCODES FUNCTION DECLARATION*/
-        uint16_t SYS (uint16_t addr);               // 0nnn - SYS addr
-        uint16_t CLS ();                            // 00E0 - CLS
-        uint16_t RET ();                            // 00EE - RET
-        uint16_t JP  (uint16_t& addr);              // 1nnn - JP addr
-        uint16_t CALL(uint16_t addr);               // 2nnn - CALL addr
-        uint16_t SE  (uint8_t VX, uint8_t byte);    // 3xkk - SE Vx, byte
-        uint16_t SNE (uint8_t VX, uint8_t byte);    // 4xkk - SNE Vx, byte
-        uint16_t SE  (uint8_t VX, uint8_t VY);      // 5xy0 - SE Vx, Vy  
-        uint16_t LD  (uint8_t& VX, uint8_t byte);   // 6xkk - LD Vx, byte
-        uint16_t ADD (uint8_t& VX, uint8_t byte);   // 7xkk - ADD Vx, byte
-        uint16_t LD  (uint8_t& VX, uint8_t VY);     // 8xy0 - LD Vx, Vy 
-        uint16_t OR  (uint8_t& VX, uint8_t& VY);    // 8xy1 - OR Vx, Vy 
-        uint16_t AND (uint8_t& VX, uint8_t& VY);    // 8xy2 - AND Vx, Vy 
-        uint16_t XOR (uint8_t& VX, uint8_t& VY);    // 8xy3 - XOR Vx, Vy 
-        uint16_t ADD (uint8_t& VX, uint8_t VY);     // 8xy4 - ADD Vx, Vy 
-        uint16_t SUB (uint8_t& VX, uint8_t VY);     // 8xy5 - SUB Vx, Vy 
-        uint16_t SHR (uint8_t& VX);                 // 8xy6 - SHR Vx {, Vy}
-        uint16_t SUBN(uint8_t& VX, uint8_t VY);     // 8xy7 - SUBN Vx, Vy
-        uint16_t SHL (uint8_t& VX);                 // 8xyE - SHL Vx {, Vy}
-        uint16_t SNE (uint8_t VX, uint8_t VY);      // 9xy0 - SNE Vx, Vy
+            uint16_t Op0();
+            uint16_t Op1();
+            uint16_t Op2();
+            uint16_t Op3();
+            uint16_t Op4();
+            uint16_t Op5();
+            uint16_t Op6();
+            uint16_t Op7();
+            uint16_t Op8();
+            uint16_t Op9();
 
+        // FUNCTIONS CALLERS/PARAMTES
+            bool Execute(uint8_t& Clock);
+            uint16_t Fetch();
 
-        /*Functions to call*/
-        bool Execute(uint8_t& Clock);
-        void Fetch_Function(uint8_t& PC);
-        void Fetch_Params(uint8_t& PC);
+            uint8_t ParamsV[3];
+            uint8_t FunctI;
+            uint8_t Clock;
 
-        uint8_t FunctionOP = 0x0;
+        // OPCODE TABLE FUNCTIONS
+            std::vector<uint16_t(*)(void)> OPTable;
 
-        /*OPCODE TABLE FUNCTIONS*/
-        std::vector<uint16_t(*)(void)> OPTable;
+        // CLOCK functions request and deecrement
+            uint8_t RequestClock();
+            uint8_t ReduceClock(uint8_t amountClock = 3);
 };
--- Source/CHIP-8/CPU.cpp
+++ Source/CHIP-8/CPU.cpp
@@ -7,40 +7,61 @@ CPU::CPU()
 {
     BUS bus;
 
-    PC = 0x0;
+    PC = 0x64;
     SP = 0x160;
 
     CPU::OPTable = {
-        &CPU::SYS, &CPU::CLS, &CPU::RET,
-        &CPU::JP,  &CPU::CALL,&CPU::SE,
-        &CPU::SNE, &CPU::SE,  &CPU::LD,
-        &CPU::ADD, &CPU::LD,  &CPU::OR,
-        &CPU::AND, &CPU::XOR, &CPU::ADD,
-        &CPU::SUB, &CPU::SHR, &CPU::SUBN,
-        &CPU::SHL, &CPU::SNE
+        &CPU::Op0, &CPU::Op1, &CPU::Op2,
+        &CPU::Op3, &CPU::Op4, &CPU::Op5,
+        &CPU::Op6, &CPU::Op7, &CPU::Op8,
+        &CPU::Op9
     };
 
 }
 
 bool CPU::Execute(uint8_t& Clock)
 {
-    uint8_t func = 0x0;
+    uint8_t FuncI = 0x0;
+    uint16_t ParamsV = 0x0;
 
     while (Clock != 0)
     {
-        // Must Fetch function
-        func = bus.Fetch_Mem(PC, DevicesConn::Stack);
+        // Request a clock
+        RequestClock();
 
-        // Must Fetch data
-        // Reduce 1 into clock
-        // Read next
+        // Fetch functions and set bytes paramters
+        Fetch();
+
+        // Call function in opcode
+        OPTable[FuncI]();
+
+        // Reduce Clock
+        ReduceClock();
     }
-    return Clock == 0;
 }
 
+uint16_t CPU::Fetch(){
+
+}
+
+uint8_t CPU::RequestClock()
+{
+    if (Clock == 0)
+    {
+        Clock += 3;
+    }
+    return 0x0;
+}
+uint8_t CPU::ReduceClock(uint8_t amountClock = 3)
+{
+    if (Clock + 3 >= 3)
+    {
+        Clock -= 3;
+    }
+}
 
 /* OPCODE FUNCTION DECLARED (ALU)*/
-uint16_t CPU::Op0(uint32_t& bytes)
+uint16_t CPU::Op0()
 {
     uint8_t op = 0x0;
 
@@ -64,76 +85,76 @@ uint16_t CPU::Op0(uint32_t& bytes)
         break;
     }
 }
-uint16_t CPU::Op1(uint32_t& bytes) // (JP) Jumps to cetain address in memory
+
+uint16_t CPU::Op1() // (JP) Jumps to cetain address in memory
 {
-    uint16_t addr;
+    uint16_t addr = (ParamsV << 16) & 0xFF;
 
     PC = addr;
     return 0x0;
 }
-uint16_t CPU::Op2(uint32_t& byte) // (CALL) Call a cetain label
+uint16_t CPU::Op2() // (CALL) Call a cetain label
 {
-    uint16_t addr;
+    uint16_t addr = (ParamsV << 16) & 0xFF;
 
     PC = addr;
     return 0x0;
 }
 
-uint16_t CPU::Op3(uint32_t& byte) // (SE) If VX is equal to kk(byte)
+uint16_t CPU::Op3() // (SE) If VX is equal to kk(byte)
 {
-    uint8_t VX;
+    // 0x12 34 56 78
+    uint8_t VX = (ParamsV >> 24) & 0xFFFF;
     uint8_t byteVal;
 
     if((VX - byteVal) == 0x0)
     {
-        PC += 2;
+        PC++;
         return 0x0;
     }
     PC++;
 }
 
-uint16_t CPU::Op4(uint32_t& byte) // (SNE) If not VX equal to kk(byte)
+uint16_t CPU::Op4() // (SNE) If not VX equal to kk(byte)
 {
     uint8_t VX;
     uint8_t byteVal;
 
     if((VX - byteVal) != 0x0)
     {
-        PC += 2;
+        PC++;
         return 0x0;
     }
     PC++;
 }
-uint16_t CPU::Op5(uint32_t& byte){
+uint16_t CPU::Op5(){
     // (SE) If VX is equal to VY
     uint8_t VX;
     uint8_t VY;
 
-    if((VX - VY) == 0x0)
+    if((VRegs[VX] - VRegs[VY]) == 0x0)
     {
-        PC += 2;
+        PC++;
     }
     return 0x0;
 }
-uint16_t CPU::Op6(uint32_t& byte){
+uint16_t CPU::Op6(){
     // (LD) Load into register VX the value kk(byte)
     uint8_t VX;
     uint8_t byteVal;
 
-    VX = byteVal;
-    PC++;
+    VRegs[VX] = byteVal;
     return 0x0;
 }
-uint16_t CPU::Op7(uint32_t& byte){
+uint16_t CPU::Op7(){
     // (ADD) Add VX register to it self and with kk(byte)
     uint8_t VX;
     uint8_t byteVal;
 
-    VX += byteVal;
-    PC++;
+    VRegs[VX] += byteVal;
     return 0x0;
 }
-uint16_t CPU::Op8(uint32_t& byte){
+uint16_t CPU::Op8(){
     uint8_t op = 0x0;
     uint8_t VX = 0x0;
     uint8_t VY = 0x0;
@@ -144,75 +165,68 @@ uint16_t CPU::Op8(uint32_t& byte){
     switch (op)
     {
     case 0x0: // (LD) Load into VX value in VX
-        VX = VY;
-        PC++;
+        VRegs[VX] = VY;
         return 0x0;
         break;
     case 0x1: // (OR) Bitwise operation (0, 1) -> 1
-        VX = VX | VY;
-        PC++;
+        VRegs[VX] = VRegs[VX] | VRegs[VY];
         return 0x0;
         break;
     case 0x2: // (AND) Bitwise operation (0, 1) -> 0
-        VX = VX & VY;
-        PC++;
+        VRegs[VX] = VRegs[VX] & VRegs[VY];
         return 0x0;
         break;
     case 0x3: // (XOR) Bitwise operation (1, 1) -> 0
-        VX = VX ^ VY;
-        PC++;
+        VRegs[VX] = VRegs[VX] ^ VRegs[VY];
         return 0x0;
         break;
     case 0x4: // (ADD) Add VX with it self with VY using Carry
-        if (VX + VY > 0x255 || VX + VY < 0x0)
+        if (VRegs[VX] + VRegs[VY] > 0x255 || VRegs[VX] + VRegs[VY] < 0x0)
         {
             VRegs[15] = 0x1;
-            VX += VY;
+            VRegs[VX] += VRegs[VY];
         }
-        PC++;
         return 0x0;
         break;
     case 0x5: // (SUB) Add VX with it self with VY using Carry
-        VX -= VY;
-        VRegs[15] = VX > VY ? 0x1 : 0x0;
-        PC++;
+        VRegs[VX] -= VRegs[VY];
+        VRegs[15] = VRegs[VX] > VRegs[VY] ? 0x1 : 0x0;
+        
         return 0x0;
         break;
     case 0x6: // (SHR) Check least significant bit in VX, if so VF = 1
-        if ((~(VX) & 0x00000001) == 0x0)
+        if ((~(VRegs[VX]) & 0x00000001) == 0x0)
         {
             VRegs[15] = 0x1;
         }
-        PC++;
+
         return 0x0;
         break;
     case 0x7: // (SUBN) Sub VX with VY, if VX ¿ VY then VF = 1
-        VX -= VY;
-        VRegs[15] = VX < VY ? 0x1 : 0x0;
-        PC++;
+        VRegs[VX] -= VRegs[VY];
+        VRegs[15] = VRegs[VX] < VRegs[VY] ? 0x1 : 0x0;
+
         return 0x0;
         break;
     case 0xE: // (SHR) Check most significant bit in VX, if so VF = 1
-        if ((~(VX) & 0x10000000) == 0x0)
+        if ((~(VRegs[VX]) & 0x10000000) == 0x0)
         {
             VRegs[15] = 0x1;
         }
-        PC++;
         return 0x0;
         break;
     default: // (XXX) Non exist function treatment
         break;
     }
 }
-uint16_t CPU::Op9(uint32_t& byte){
+uint16_t CPU::Op9(){
     // (SNE) If not, VX is different of VY
     uint8_t VX;
     uint8_t VY;
 
-    if ((VX - VY) != 0x0)
+    if ((VRegs[VX] - VRegs[VY]) != 0x0)
     {
-        PC += 2;
+        PC++;
         return 0x0;
     }
-    PC++;
 }
